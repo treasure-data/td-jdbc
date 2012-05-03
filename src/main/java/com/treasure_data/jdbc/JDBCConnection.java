fecdd004c29d1bceff727a1e7c49d68f7b3031dc
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-05-02 17:05:35 -0700

    renamed some classes: JDBCConnection and JDBCStatement classes

--- src/main/java/com/treasure_data/jdbc/JDBCConnection.java
+++ src/main/java/com/treasure_data/jdbc/JDBCConnection.java
@@ -17,14 +17,11 @@ import java.sql.Statement;
 import java.sql.Struct;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 import com.treasure_data.client.TreasureDataClient;
 import com.treasure_data.model.Database;
 
-public class TreasureDataConnection implements Connection, TDConstants {
-    private static Logger LOG = Logger.getLogger(
-            TreasureDataConnection.class.getName());
+public class JDBCConnection implements Connection, Constants {
 
     private TreasureDataClient client;
 
@@ -32,362 +29,207 @@ public class TreasureDataConnection implements Connection, TDConstants {
 
     private SQLWarning warnings = null;
 
-    public TreasureDataConnection(String uri, Properties props)
+    public JDBCConnection(String uri, Properties props)
             throws SQLException {
-        // TODO #MN
-        if (!uri.startsWith(URI_PREFIX)) {
-            throw new SQLException("Invalid URL: " + uri, "08S01");
+        if (uri == null || uri.isEmpty() || !uri.startsWith(URI_PREFIX)) {
+            throw new SQLException("Invalid URL: " + uri);
         }
 
         // remove prefix
-        uri = uri.substring(TreasureDataDriver.URI_PREFIX.length());
-
+        uri = uri.substring(Constants.URI_PREFIX.length());
         if (uri.isEmpty()) {
-            SQLException e = new SQLException(
-                    "Error accessing Treasure Data Cloud", "08S01");
-            warnings.setNextException(e);
-            throw e;
-        } else {
-            // parse uri form: host:port/db
-            String[] parts = uri.split("/");
-            String[] hostport = parts[0].split(":");
-            int port = 80;
-            String host = hostport[0];
-            try {
-                port = Integer.parseInt(hostport[1]);
-            } catch (Exception e) {
-            }
-            props.setProperty(com.treasure_data.client.Config.TD_API_SERVER_HOST,
-                    host + ":" + port);
-            client = new TreasureDataClient(props);
-
-            if (parts.length > 1) {
-                database = new Database(parts[1]);
-            }
+            throw new SQLException("Error accessing Treasure Data Cloud");
         }
+
+        // parse uri form: host:port/db
+        String[] fragments = uri.split("/");
+        String[] hostport = fragments[0].split(":");
+        int port = 80;
+        String host = hostport[0];
+        try {
+            port = Integer.parseInt(hostport[1]);
+        } catch (Exception e) {
+            // *ignore*: if an exception is thrown, 80 is
+            // inserted into a port variable. 
+        }
+        props.setProperty(Config.TD_API_SERVER_HOST, host + ":" + port);
+
+        if (fragments.length > 1) {
+            database = new Database(fragments[1]);
+        } else {
+            throw new SQLException(
+            "Can't create a connection because database is not specified");
+        }
+
+        client = new TreasureDataClient(props);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#clearWarnings()
-     */
     public void clearWarnings() throws SQLException {
         warnings = null;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#close()
-     */
+    public SQLWarning getWarnings() throws SQLException {
+        return warnings;
+    }
+
+    public void setWarning(SQLWarning w) throws SQLException {
+        if (w == null) {
+            throw new SQLException("a SQLWarning object is null");
+        }
+
+        if (warnings == null) {
+            warnings = w;
+        } else {
+            warnings.setNextWarning(w);
+        }
+    }
+
     public void close() throws SQLException {
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#commit()
-     */
+    public boolean isClosed() throws SQLException {
+        return true;
+    }
+
     public void commit() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createArrayOf(java.lang.String,
-     * java.lang.Object[])
-     */
     public Array createArrayOf(String arg0, Object[] arg1) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createBlob()
-     */
     public Blob createBlob() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createClob()
-     */
     public Clob createClob() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createNClob()
-     */
     public NClob createNClob() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createSQLXML()
-     */
     public SQLXML createSQLXML() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
     /**
-     * Creates a Statement object for sending SQL statements to the database.
+     * Creates a Statement object for sending SQL statements to
+     * the database.
      * 
      * @throws SQLException
      *           if a database access error occurs.
      * @see java.sql.Connection#createStatement()
      */
     public Statement createStatement() throws SQLException {
-        if (database == null) {// TODO #MN
-            throw new SQLException("Can't create Statement, database is not specified");
-        }
-        return new TreasureDataStatement(client, database);
+        // *skip* checking if database is null or not. It is because
+        // it was processed when creating a connection.
+        return new JDBCStatement(client, database); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createStatement(int, int)
-     */
     public Statement createStatement(int resultSetType, int resultSetConcurrency)
             throws SQLException {
-        throw new SQLException("Method not supported");
+        return new JDBCStatement(client, database); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createStatement(int, int, int)
-     */
     public Statement createStatement(int resultSetType, int resultSetConcurrency,
             int resultSetHoldability) throws SQLException {
-        throw new SQLException("Method not supported");
+        return new JDBCStatement(client, database); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#createStruct(java.lang.String, java.lang.Object[])
-     */
     public Struct createStruct(String typeName, Object[] attributes)
             throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getAutoCommit()
-     */
     public boolean getAutoCommit() throws SQLException {
         return true;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getCatalog()
-     */
     public String getCatalog() throws SQLException {
         return "";
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getClientInfo()
-     */
     public Properties getClientInfo() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getClientInfo(java.lang.String)
-     */
     public String getClientInfo(String name) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getHoldability()
-     */
     public int getHoldability() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getMetaData()
-     */
     public DatabaseMetaData getMetaData() throws SQLException {
         return new TreasureDataDatabaseMetaData(client);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getTransactionIsolation()
-     */
     public int getTransactionIsolation() throws SQLException {
         return Connection.TRANSACTION_NONE;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getTypeMap()
-     */
     public Map<String, Class<?>> getTypeMap() throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#getWarnings()
-     */
-    public SQLWarning getWarnings() throws SQLException {
-        return warnings;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#isClosed()
-     */
-    public boolean isClosed() throws SQLException {
-        return true;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#isReadOnly()
-     */
     public boolean isReadOnly() throws SQLException {
         return false;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#isValid(int)
-     */
     public boolean isValid(int timeout) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#nativeSQL(java.lang.String)
-     */
     public String nativeSQL(String sql) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareCall(java.lang.String)
-     */
     public CallableStatement prepareCall(String sql) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareCall(java.lang.String, int, int)
-     */
     public CallableStatement prepareCall(String sql, int resultSetType,
             int resultSetConcurrency) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareCall(java.lang.String, int, int, int)
-     */
     public CallableStatement prepareCall(String sql, int resultSetType,
             int resultSetConcurrency, int resultSetHoldability) throws SQLException {
         throw new SQLException("Method not supported");
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String)
-     */
     public PreparedStatement prepareStatement(String sql) throws SQLException {
-        return new TreasureDataPreparedStatement(client, database, sql);
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String, int)
-     */
     public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys)
             throws SQLException {
-        return new TreasureDataPreparedStatement(client, database, sql);
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String, int[])
-     */
     public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
             throws SQLException {
-        throw new SQLException("Method not supported");
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String,
-     * java.lang.String[])
-     */
     public PreparedStatement prepareStatement(String sql, String[] columnNames)
             throws SQLException {
-        throw new SQLException("Method not supported");
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String, int, int)
-     */
     public PreparedStatement prepareStatement(String sql, int resultSetType,
         int resultSetConcurrency) throws SQLException {
-        return new TreasureDataPreparedStatement(client, database, sql);
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.sql.Connection#prepareStatement(java.lang.String, int, int, int)
-     */
     public PreparedStatement prepareStatement(String sql, int resultSetType,
             int resultSetConcurrency, int resultSetHoldability)
             throws SQLException {
-        throw new SQLException("Method not supported");
+        return new TreasureDataPreparedStatement(client, database, sql); // TODO
     }
 
     /*

1b7d16a655ea2a4f9915fa98a9bd2c7ea802acd0
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-05-18 18:08:23 -0700

    added new test case for TDClientAPI class

---
+++ src/test/java/com/treasure_data/jdbc/command/TestTDClientAPI.java
@@ -0,0 +1,13 @@
+package com.treasure_data.jdbc.command;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestTDClientAPI {
+
+    @Test
+    public void testDrop() throws Exception {
+        assertTrue(true);
+    }
+}

673742d70d4d8ac90c6650a2c18b5d7f2c8d2161
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-05-19 18:41:01 -0700

    added new test method for TDResultSet#next()

--- src/test/java/com/treasure_data/jdbc/TestTDResultSet.java
+++ src/test/java/com/treasure_data/jdbc/TestTDResultSet.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.json.simple.JSONValue;
 import org.junit.Test;
 import org.msgpack.MessagePack;
 import org.msgpack.packer.BufferPacker;
@@ -54,6 +53,42 @@ public class TestTDResultSet {
 
         public Unpacker getJobResult(Job job) throws ClientException {
             return null;
+        }
+    }
+
+    @Test
+    public void testNext01() throws Exception {
+        ClientAPI clientApi = new MockClientAPI() {
+            public JobSummary waitJobResult(Job job) throws ClientException {
+                String resultSchema = "[[\"age\",\"int\"],[\"name\",\"string\"]]";
+                return new JobSummary("12345", JobSummary.Type.HIVE, new Database("mugadb"),
+                        "url", "rtbl", Status.SUCCESS, "startAt", "endAt", "query", resultSchema);
+            }
+
+            public Unpacker getJobResult(Job job) throws ClientException {
+                try {
+                    MessagePack msgpack = new MessagePack();
+                    BufferPacker packer = msgpack.createBufferPacker();
+                    List<Object> ret0 = new ArrayList<Object>();
+                    ret0.add(10);
+                    ret0.add("muga");
+                    packer.write(ret0);
+                    List<Object> ret1 = new ArrayList<Object>();
+                    ret1.add(20);
+                    ret1.add("nishizawa");
+                    packer.write(ret1);
+                    byte[] bytes = packer.toByteArray();
+                    return msgpack.createBufferUnpacker(bytes);
+                } catch (java.io.IOException e) {
+                    throw new ClientException("mock");
+                }
+            }
+        };
+        Job job = new Job("12345");
+        ResultSet rs = new TDResultSet(clientApi, 50, job);
+        while (rs.next()) {
+            System.out.println(rs.getInt(1));
+            System.out.println(rs.getString(2));
         }
     }
 
@@ -252,45 +287,6 @@ public class TestTDResultSet {
             } catch (Throwable t) {
                 assertTrue(t instanceof IndexOutOfBoundsException);
             }
-        }
-    }
-
-    @Test
-    public void testSample() throws Exception {
-        ClientAPI clientApi = new MockClientAPI() {
-            public JobSummary waitJobResult(Job job) throws ClientException {
-                String resultSchema = "[[\"age\",\"int\"],[\"name\",\"string\"]]";
-                return new JobSummary("12345", JobSummary.Type.HIVE, new Database("mugadb"),
-                        "url", "rtbl", Status.SUCCESS, "startAt", "endAt", "query", resultSchema);
-            }
-
-            public Unpacker getJobResult(Job job) throws ClientException {
-                List<List<Object>> result = new ArrayList<List<Object>>();
-                List<Object> ret0 = new ArrayList<Object>();
-                ret0.add(10);
-                ret0.add("muga");
-                result.add(ret0);
-                List<Object> ret1 = new ArrayList<Object>();
-                ret1.add(20);
-                ret1.add("nishizawa");
-                result.add(ret1);
-
-                try {
-                    MessagePack msgpack = new MessagePack();
-                    BufferPacker packer = msgpack.createBufferPacker();
-                    packer.write(result);
-                    byte[] bytes = packer.toByteArray();
-                    return msgpack.createBufferUnpacker(bytes);
-                } catch (java.io.IOException e) {
-                    throw new ClientException("mock");
-                }
-            }
-        };
-        Job job = new Job("12345");
-        ResultSet rs = new TDResultSet(clientApi, 50, job);
-        while (rs.next()) {
-            System.out.println(rs.getObject(1));
-            System.out.println(rs.getObject(2));
         }
     }
 }

d5be74e8322d08782d9721dc3d50516ad31756bc
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-06-04 16:02:18 -0700

    changed CommandContext class according to the implementation of the executeBatch method

--- src/main/java/com/treasure_data/jdbc/command/CommandContext.java
+++ src/main/java/com/treasure_data/jdbc/command/CommandContext.java
@@ -1,8 +1,11 @@
 package com.treasure_data.jdbc.command;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import com.treasure_data.jdbc.compiler.stat.Statement;
 
 public class CommandContext {
 
@@ -10,11 +13,13 @@ public class CommandContext {
 
     public String sql;
 
-    public com.treasure_data.jdbc.compiler.stat.Statement compiledSql;
+    public Statement compiledSql;
 
     public List<String> paramList;
 
     public Map<Integer, Object> params;
+
+    public List<Map<Integer, Object>> params0 = new ArrayList<Map<Integer, Object>>();
 
     public ResultSet resultSet;
 

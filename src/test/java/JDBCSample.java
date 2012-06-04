d5a549d866097fc15cb9cf1026f6e3fada7f68a3
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-06-04 16:00:56 -0700

    changed sample programs

--- src/test/java/JDBCSample.java
+++ src/test/java/JDBCSample.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
+
+import com.treasure_data.jdbc.TDPreparedStatement;
+import com.treasure_data.logger.TreasureDataLogger;
 
 public class JDBCSample {
     public static void loadSystemProperties() throws IOException {
@@ -31,8 +35,35 @@ public class JDBCSample {
         Connection conn = DriverManager.getConnection(
                 //"jdbc:td://192.168.0.23:80/mugadb", user, password);
                 "jdbc:td://api.treasure-data.com:80/mugatest", user, password);
+        String sql = "insert into table4 (time) values (?)";
+        TDPreparedStatement stmt = (TDPreparedStatement) conn.prepareStatement(sql);
+        for (int i = 0; i < 10; i++) {
+            stmt.setString(1, "muga:" + i);
+            stmt.addBatch();
+        }
+        stmt.executeBatch();
+
+        System.out.println("Running: " + stmt);
+        TreasureDataLogger.flushAll();
+    }
+
+    public static void main2(String[] args) throws Exception {
+        loadSystemProperties();
+
+        try {
+            Class.forName("com.treasure_data.jdbc.TreasureDataDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String user = getSystemProperty("td.api.user");
+        String password = getSystemProperty("td.api.password");
+        Connection conn = DriverManager.getConnection(
+                //"jdbc:td://192.168.0.23:80/mugadb", user, password);
+                "jdbc:td://api.treasure-data.com:80/mugadb", user, password);
         Statement stmt = conn.createStatement();
-        String sql = "select cast(count(*) as double) from table2";
+        String sql = "select v['score'] as score, v['id'] as id, v['name'] as name from score order by score desc";
         System.out.println("Running: " + sql);
         ResultSet res = stmt.executeQuery(sql);
         while (res.next()) {

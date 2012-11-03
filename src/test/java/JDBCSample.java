d55b9ee6f5ee77fbac25f20f332013f70ee7dc71
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-11-03 15:46:28 +0900

    changed JDBCSample class

--- src/test/java/JDBCSample.java
+++ src/test/java/JDBCSample.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -20,7 +22,7 @@ public class JDBCSample {
         return props.getProperty(key);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void insertSample() throws Exception {
         loadSystemProperties();
 
         try {
@@ -47,7 +49,7 @@ public class JDBCSample {
         TreasureDataLogger.flushAll();
     }
 
-    public static void main2(String[] args) throws Exception {
+    public static void selectSample() throws Exception {
         loadSystemProperties();
 
         try {
@@ -60,14 +62,48 @@ public class JDBCSample {
         String user = getSystemProperty("td.api.user");
         String password = getSystemProperty("td.api.password");
         Connection conn = DriverManager.getConnection(
-                //"jdbc:td://192.168.0.23:80/mugadb", user, password);
                 "jdbc:td://api.treasure-data.com:80/mugadb", user, password);
         Statement stmt = conn.createStatement();
+        stmt.setQueryTimeout(5);
         String sql = "select v['score'] as score, v['id'] as id, v['name'] as name from score order by score desc";
+        //String sql = "select v from score";
         System.out.println("Running: " + sql);
         ResultSet res = stmt.executeQuery(sql);
-        while (res.next()) {
-            System.out.println(String.valueOf(res.getObject(1)));
+        try {
+            while (res.next()) {
+                //System.out.println(String.valueOf(res.getObject(1)));
+                System.out.println(res.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            res.close();
         }
+    }
+
+    public static void dataabseMetadataSample() throws Exception {
+        //loadSystemProperties();
+
+        try {
+            Class.forName("com.treasure_data.jdbc.TreasureDataDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String user = getSystemProperty("td.api.user");
+        String password = getSystemProperty("td.api.password");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:td://localhost:9999/mugadb", "k@treasure-data.com", "treasure311");
+                //"jdbc:td://api.treasure-data.com:80/mugadb", user, password);
+        DatabaseMetaData dmd = conn.getMetaData();
+        ResultSet rs = dmd.getColumns(null, null, "score", null);
+        while (rs.next()) {
+            System.out.println(rs.getString("TABLE_NAME"));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        selectSample();
     }
 }

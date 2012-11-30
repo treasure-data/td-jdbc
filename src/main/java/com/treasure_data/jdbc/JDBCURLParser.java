a937340fd5011570d8a8680f8ead6aebd99f0b6e
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-12-01 03:34:16 +0900

    added new class JDBCURLParser, which is a parser program for JDBC URL

---
+++ src/main/java/com/treasure_data/jdbc/JDBCURLParser.java
@@ -0,0 +1,101 @@
+package com.treasure_data.jdbc;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+//
+// jdbc:td://api.treasure-data.com:80/testdb;k1=v1;k2=v2
+// +-------+ +-------------------+ ++ +----+ +---------+
+// type      host               port  path   parameters
+//           +----------------------+
+//           endpoint
+//
+public class JDBCURLParser {
+
+    static class Desc {
+        String url = null;
+        String host = Constants.TD_JDBC_HOST_DEFAULT;
+        String port = Constants.TD_JDBC_PORT_DEFAULT;
+        String database = null;
+        String user = null;
+        String password = null;
+
+        public Desc() {
+        }
+    }
+
+    public static Desc parse(String url) throws SQLException {
+        Utils.validateJDBCType(url);
+
+        Desc d = new Desc();
+
+        d.url = url;
+
+        int postUrlPos = url.length();
+        // postUrlPos is the END position in url String,                                                                                                                                                                                                        
+        // wrt what remains to be processed.                                                                                                                                                                                                                    
+        // i.e., if postUrlPos is 100, url no longer needs to examined at                                                                                                                                                                                       
+        // index 100 or later.                                                                                                                                                                                                                                  
+        int semiPos = url.indexOf(';', Constants.URL_PREFIX.length());
+        if (semiPos > -1) {
+            String params = url.substring(semiPos + 1, postUrlPos);
+            String[] kvs = params.split(";");
+            for (String param : kvs) {
+                String[] kv = param.split("=");
+                if (kv == null || kv.length != 2) {
+                    throw new SQLException("invalid parameters within URL: " + url);
+                }
+
+                String k = kv[0];
+                if (k.equals("user")) {
+                    d.user = kv[1];
+                } else if (k.equals("password")) {
+                    d.password = kv[1];
+                }
+            }
+        } else {
+            semiPos = postUrlPos;
+        }
+
+        int slashPos = url.indexOf('/', Constants.URL_PREFIX.length());
+        if (slashPos >= 0) {
+            try {
+                String rawDatabase = url.substring(slashPos + 1, semiPos);
+                if (rawDatabase != null && !rawDatabase.isEmpty()) {
+                    d.database = rawDatabase;
+                }
+            } catch (Throwable t) {
+                throw new SQLException("invalid database within URL: " + url);
+            }
+        }
+        if (d.database == null) {
+            throw new SQLException("Not found database within URL: " + url);
+        }
+
+        String endpoint = null;
+        try {
+            endpoint = url.substring(Constants.URL_PREFIX.length(), slashPos);
+        } catch (Throwable t) {
+            throw new SQLException("invalid endpoint within URL: " + url);
+        }
+        if (endpoint != null && !endpoint.isEmpty()) {
+            int i = endpoint.indexOf(':');
+            if (i >= 0) {
+                if (i != 0) {
+                    d.host = endpoint.substring(0, i);
+                }
+                d.port = endpoint.substring(i + 1, endpoint.length());
+                try {
+                    Integer.parseInt(d.port);
+                } catch (Throwable t) {
+                    throw new SQLException("invalid port within URL: " + url);
+                }
+            } else { // i < 0
+                d.host = endpoint;
+            }
+        }
+        
+        return d;
+    }
+
+}
---
+++ src/test/java/com/treasure_data/jdbc/TestJDBCURLParser.java
@@ -0,0 +1,150 @@
+package com.treasure_data.jdbc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class TestJDBCURLParser {
+    @Test
+    public void testFull() throws Exception {
+        final String url = "jdbc:td://host01:9999/db01;user=user01;password=pass01;k01=v01;k02=v02";
+        JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+        assertEquals(url, d.url);
+        assertEquals("host01", d.host);
+        assertEquals("9999", d.port);
+        assertEquals("db01", d.database);
+        assertEquals("user01", d.user);
+        assertEquals("pass01", d.password);
+    }
+
+    @Test
+    public void testType() throws Exception {
+        final String url = "jdbc:td://";
+        try {
+            JDBCURLParser.parse(url);
+            fail();
+        } catch (Throwable t) {
+            assertTrue(t instanceof SQLException);
+        }
+    }
+
+    @Test
+    public void testInvalidType() throws Exception {
+        final String url = "odbc:td://host01:9999/db01;user=user01;password=pass01;k01=v01;k02=v02";
+        try {
+            JDBCURLParser.parse(url);
+            fail();
+        } catch (Throwable t) {
+            assertTrue(t instanceof SQLException);
+        }
+    }
+
+    @Test
+    public void testNoHost() throws Exception {
+        final String url = "jdbc:td://:9999/db01;user=user01;password=pass01;k01=v01;k02=v02";
+        JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+        assertEquals(url, d.url);
+        assertEquals("api.treasure-data.com", d.host);
+        assertEquals("9999", d.port);
+        assertEquals("db01", d.database);
+        assertEquals("user01", d.user);
+        assertEquals("pass01", d.password);
+    }
+
+    @Test
+    public void testNoPort() throws Exception {
+        final String url = "jdbc:td://host01/db01;user=user01;password=pass01;k01=v01;k02=v02";
+        JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+        assertEquals(url, d.url);
+        assertEquals("host01", d.host);
+        assertEquals("80", d.port);
+        assertEquals("db01", d.database);
+        assertEquals("user01", d.user);
+        assertEquals("pass01", d.password);
+    }
+
+    @Test
+    public void testInvalidPort() throws Exception {
+        {
+            final String url = "jdbc:td://host01:/db01;user=user01;password=pass01;k01=v01;k02=v02";
+            try {
+                JDBCURLParser.parse(url);
+                fail();
+            } catch (Throwable t) {
+                assertTrue(t instanceof SQLException);
+            }
+        }
+        {
+            final String url = "jdbc:td://host01:str/db01;user=user01;password=pass01;k01=v01;k02=v02";
+            try {
+                JDBCURLParser.parse(url);
+                fail();
+            } catch (Throwable t) {
+                assertTrue(t instanceof SQLException);
+            }
+        }
+    }
+
+    @Test
+    public void testNoHostAndPort() throws Exception {
+        final String url = "jdbc:td:///db01;user=user01;password=pass01;k01=v01;k02=v02";
+        JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+        assertEquals(url, d.url);
+        assertEquals("api.treasure-data.com", d.host);
+        assertEquals("80", d.port);
+        assertEquals("db01", d.database);
+        assertEquals("user01", d.user);
+        assertEquals("pass01", d.password);
+    }
+
+    @Test
+    public void testNoDatabase() throws Exception {
+        {
+            final String url = "jdbc:td://host01;user=user01;password=pass01;k01=v01;k02=v02";
+            try {
+                JDBCURLParser.parse(url);
+                fail();
+            } catch (Throwable t) {
+                assertTrue(t instanceof SQLException);
+            }
+        }
+        {
+            final String url = "jdbc:td://host01/;user=user01;password=pass01;k01=v01;k02=v02";
+            try {
+                JDBCURLParser.parse(url);
+                fail();
+            } catch (Throwable t) {
+                assertTrue(t instanceof SQLException);
+            }
+        }
+    }
+
+    @Test
+    public void testNoParameters() throws Exception {
+        {
+            final String url = "jdbc:td://host01:9999/db01";
+            JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+            assertEquals(url, d.url);
+            assertEquals("host01", d.host);
+            assertEquals("9999", d.port);
+            assertEquals("db01", d.database);
+            assertEquals(null, d.user);
+            assertEquals(null, d.password);
+        }
+        {
+            final String url = "jdbc:td:///db01";
+            JDBCURLParser.Desc d = JDBCURLParser.parse(url);
+            assertEquals(url, d.url);
+            assertEquals("api.treasure-data.com", d.host);
+            assertEquals("80", d.port);
+            assertEquals("db01", d.database);
+            assertEquals(null, d.user);
+            assertEquals(null, d.password);
+        }
+    }
+}
